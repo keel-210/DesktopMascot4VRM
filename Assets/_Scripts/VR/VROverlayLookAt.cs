76d30c05af9ac44f55920d508cd145808c424bef
keel-210 <crafters_touhou@yahoo.co.jp>
2018-10-28 16:49:30 +0900

    Ver0.41_VRSupport

--- Assets/_Scripts/VR/VROverlayLookAt.cs
+++ Assets/_Scripts/VR/VROverlayLookAt.cs
@@ -6,12 +6,9 @@ using UnityEngine.XR;
 public class VROverlayLookAt : MonoBehaviour
 {
 	[SerializeField] EasyOpenVROverlayForUnity OVO;
-
 	void Update()
 	{
-		Vector3 EyePos = InputTracking.GetLocalPosition(XRNode.CenterEye);
-        Debug.Log(EyePos);
-		Quaternion rot = Quaternion.LookRotation(OVO.Position,Vector3.up);
-		OVO.Rotation = new Vector3(0,-rot.eulerAngles.y,0);
+		Quaternion rot = Quaternion.LookRotation(OVO.Position, Vector3.up);
+		OVO.Rotation = new Vector3(0, -rot.eulerAngles.y, 0);
 	}
 }
