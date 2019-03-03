01022947c927580eb30004638b80b057e750ab4b
keel-210 <crafters_touhou@yahoo.co.jp>
2019-03-03 20:39:56 +0900

    VroidHub_Support_ver0.01
    一応Vroid関連のコードは秘匿

---
+++ Assets/_Scripts/Test/MultiSceneTest.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiSceneTest : MonoBehaviour
+{
+    void Start()
+    {
+        GameObject obj = FindObjectOfType<Quote>().gameObject;
+        Debug.Log(obj?.name);
+    }
+}
